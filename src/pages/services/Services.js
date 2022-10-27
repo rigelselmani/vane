b00a64e1444de1cad0dce4ae44487bb343a553d3
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-10-27 13:17:31 -0400

    services page and style including the text

--- src/pages/services/Services.js
+++ src/pages/services/Services.js
@@ -2,8 +2,54 @@ import "./services.css"
 
 export default function Services (){
     return(
-        <div className="container ser mt-5">
-            Services
+        <div className="container my-5">
+            <div className="card" >
+                 <div className="card-body">
+                 <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2019_51/1087901/manicure-nail-salon-today-160513-tease-03.jpg" className="card-img-top" alt="..." />
+                   <h5 className="card-title">
+                     <span className="col-6">MANICURE SERVICES</span>
+                   </h5>
+                   <p className="card-text ">
+                     <span className=" col-4">Pure Basic Manicure (Reg)_ _ _$25 </span><br/>
+                     <span className=" col-7">Pure Basic Manicure (Gel)_ _ _$30 </span><br/>
+                     <span className=" col-4">Pure Deluxe Spa Manicure (Reg, Polish)_ _ _$35 </span><br/>
+                     <span className=" col-6">My First Manicure (for Kids)_ _ _ $10 </span><br/>
+                     <span className=" col-8">(For Kids) My First Pedicure_ _ _$20 </span>
+                   </p>
+                 </div>
+            </div>
+            <div className="card mx-1" >
+                 <div className="card-body">
+                 <img src="https://i.ytimg.com/vi/AEKGC4ccBsw/maxresdefault.jpg" className="card-img-top" alt="..." />
+                   <h5 className="card-title">
+                     <span className="col-6">MANICURE SERVICES</span>
+                   </h5>
+                   <p className="card-text ">
+                     <span className=" col-4">Pure Basic Pedicure Regular Polish_ _ _$35  </span><br/>
+                     <span className=" col-7">Pure Basic Pedicure Regular Polish (Gel)_ _ _$47  </span><br/>
+                     <span className=" col-4">Pure Deluxe Spa Pedicure_ _ _$50  </span><br/>
+                     <span className=" col-6">Pure Deluxe Spa Pedicure (Gel)_ _ _$62 </span><br/>
+                     <span className=" col-8">Pure Luxury Pedicure_ _ _$65 </span><br/>
+                     <span className=" col-8">Pure Luxury Pedicure (Gel)_ _ _$77 </span>
+                   </p>
+                 </div>
+            </div>
+            <div className="card" >
+                 <div className="card-body">
+                 <img src="https://i.ytimg.com/vi/AEKGC4ccBsw/maxresdefault.jpg" className="card-img-top" alt="..." />
+                   <h5 className="card-title">
+                     <span className="col-6">MANICURE SERVICES</span>
+                   </h5>
+                   <p className="card-text ">
+                     <span className=" col-4">Pure Basic Pedicure Regular Polish_ _ _$35  </span><br/>
+                     <span className=" col-7">Pure Basic Pedicure Regular Polish (Gel)_ _ _$47  </span><br/>
+                     <span className=" col-4">Pure Deluxe Spa Pedicure_ _ _$50  </span><br/>
+                     <span className=" col-6">Pure Deluxe Spa Pedicure (Gel)_ _ _$62 </span><br/>
+                     <span className=" col-8">Pure Luxury Pedicure_ _ _$65 </span><br/>
+                     <span className=" col-8">Pure Luxury Pedicure (Gel)_ _ _$77 </span>
+                   </p>
+                 </div>
+            </div>
         </div>
     )
 }
