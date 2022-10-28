4f6fddee6ce0cc6bba0bd5e32dab62710e57abf8
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-10-28 13:29:46 -0400

    services page text and images and positioning row and columns

--- src/pages/services/Services.js
+++ src/pages/services/Services.js
@@ -2,54 +2,105 @@ import "./services.css"
 
 export default function Services (){
     return(
-        <div className="container my-5">
-            <div className="card" >
-                 <div className="card-body">
-                 <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2019_51/1087901/manicure-nail-salon-today-160513-tease-03.jpg" className="card-img-top" alt="..." />
-                   <h5 className="card-title">
-                     <span className="col-6">MANICURE SERVICES</span>
-                   </h5>
-                   <p className="card-text ">
-                     <span className=" col-4">Pure Basic Manicure (Reg)_ _ _$25 </span><br/>
-                     <span className=" col-7">Pure Basic Manicure (Gel)_ _ _$30 </span><br/>
-                     <span className=" col-4">Pure Deluxe Spa Manicure (Reg, Polish)_ _ _$35 </span><br/>
-                     <span className=" col-6">My First Manicure (for Kids)_ _ _ $10 </span><br/>
-                     <span className=" col-8">(For Kids) My First Pedicure_ _ _$20 </span>
-                   </p>
-                 </div>
-            </div>
-            <div className="card mx-1" >
-                 <div className="card-body">
-                 <img src="https://i.ytimg.com/vi/AEKGC4ccBsw/maxresdefault.jpg" className="card-img-top" alt="..." />
-                   <h5 className="card-title">
-                     <span className="col-6">MANICURE SERVICES</span>
-                   </h5>
-                   <p className="card-text ">
-                     <span className=" col-4">Pure Basic Pedicure Regular Polish_ _ _$35  </span><br/>
-                     <span className=" col-7">Pure Basic Pedicure Regular Polish (Gel)_ _ _$47  </span><br/>
-                     <span className=" col-4">Pure Deluxe Spa Pedicure_ _ _$50  </span><br/>
-                     <span className=" col-6">Pure Deluxe Spa Pedicure (Gel)_ _ _$62 </span><br/>
-                     <span className=" col-8">Pure Luxury Pedicure_ _ _$65 </span><br/>
-                     <span className=" col-8">Pure Luxury Pedicure (Gel)_ _ _$77 </span>
-                   </p>
-                 </div>
-            </div>
-            <div className="card" >
-                 <div className="card-body">
-                 <img src="https://i.ytimg.com/vi/AEKGC4ccBsw/maxresdefault.jpg" className="card-img-top" alt="..." />
-                   <h5 className="card-title">
-                     <span className="col-6">MANICURE SERVICES</span>
-                   </h5>
-                   <p className="card-text ">
-                     <span className=" col-4">Pure Basic Pedicure Regular Polish_ _ _$35  </span><br/>
-                     <span className=" col-7">Pure Basic Pedicure Regular Polish (Gel)_ _ _$47  </span><br/>
-                     <span className=" col-4">Pure Deluxe Spa Pedicure_ _ _$50  </span><br/>
-                     <span className=" col-6">Pure Deluxe Spa Pedicure (Gel)_ _ _$62 </span><br/>
-                     <span className=" col-8">Pure Luxury Pedicure_ _ _$65 </span><br/>
-                     <span className=" col-8">Pure Luxury Pedicure (Gel)_ _ _$77 </span>
-                   </p>
-                 </div>
-            </div>
+        <div className="container  my-5">
+            <div className="row">
+                  <div className="col-1"></div>
+                  <div className="card col-3" >
+                       <div className="card-body">
+                       <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2019_51/1087901/manicure-nail-salon-today-160513-tease-03.jpg" className="card-img-top" alt="..." />
+                         <h5 className="card-title">
+                           <span className="col-6">MANICURE SERVICES</span>
+                         </h5>
+                         <p className="card-text ">
+                           <span className=" col-4">Pure Basic Manicure (Reg)_ _ _$25 </span><br/>
+                           <span className=" col-7">Pure Basic Manicure (Gel)_ _ _$30 </span><br/>
+                           <span className=" col-4">Pure Deluxe Spa Manicure (Reg, Polish)_ _ _$35 </span><br/>
+                           <span className=" col-6">My First Manicure (for Kids)_ _ _ $10 </span><br/>
+                           <span className=" col-8">(For Kids) My First Pedicure_ _ _$20 </span>
+                         </p>
+                       </div>
+                  </div>
+                  
+                  <div className="card col-3 mx-3" >
+                       <div className="card-body">
+                       <img src="https://i.ytimg.com/vi/AEKGC4ccBsw/maxresdefault.jpg" className="card-img-top" alt="..." />
+                         <h5 className="card-title">
+                           <span className="col-6">MANICURE SERVICES</span>
+                         </h5>
+                         <p className="card-text ">
+                           <span className=" col-4">Pure Basic Pedicure Regular Polish_ _ _$35  </span><br/>
+                           <span className=" col-7">Pure Basic Pedicure Regular Polish (Gel)_ _ _$47  </span><br/>
+                           <span className=" col-4">Pure Deluxe Spa Pedicure_ _ _$50  </span><br/>
+                           <span className=" col-6">Pure Deluxe Spa Pedicure (Gel)_ _ _$62 </span><br/>
+                           <span className=" col-8">Pure Luxury Pedicure_ _ _$65 </span><br/>
+                           <span className=" col-8">Pure Luxury Pedicure (Gel)_ _ _$77 </span>
+                         </p>
+                       </div>
+                  </div>
+                  <div className="card col-3" >
+                       <div className="card-body">
+                       <img src="http://5starsnailsinc.com/wp-content/uploads/2022/07/4.jpg" className="card-img-top" alt="..." />
+                         <h5 className="card-title">
+                           <span className="col-6">NAIL ENHANCEMENT Full Set</span>
+                         </h5>
+                         <p className="card-text ">
+                           <span className=" col-4">Acrylic (Regular.P)_ _ _ $35 + </span><br/>
+                           <span className=" col-7">Acrylic (Gel.Polish)_ _ _ $45 +</span><br/>
+                           <span className=" col-4">Pink & White_ _ _$50+  </span><br/>
+                           <span className=" col-6">Liquid Gel_ _ _$62 </span><br/>
+                           <span className=" col-8">Ombre Power_ _ _$65 </span><br/>
+                         </p>
+                       </div>
+                  </div>
+                  <div className="col-1"></div>
+                  <div className="col-1"></div>
+                   <div className="card col-3" >
+                       <div className="card-body">
+                       <img src="https://m.media-amazon.com/images/I/51fu9VJ3DYL._AC_SY1000_.jpg" className="card-img-top" alt="..." />
+                         <h5 className="card-title">
+                           <span className="col-6">Natural Nail System</span>
+                         </h5>
+                         <p className="card-text ">
+                           <span className=" col-4">Dipping Power Full Color On Natural Nail_ _ _ $45 +</span><br/>
+                           <span className=" col-7">Dipping Power Full Color With Tips_ _ _ $45 +</span><br/>
+                           <span className=" col-4">Dipping Power Pink & White Set_ _ _$50+  </span><br/>
+                           <span className=" col-6">Dipping Power Ombre_ _ _$62+ </span><br/>
+                         </p>
+                       </div>
+                  </div>
+                  <div className="card col-3 mx-3" >
+                       <div className="card-body">
+                       <img src="https://regalianailsandspaaustin.com/uploads/venetixibiolv/logo/2019/11/21/slide20.png" className="card-img-top" alt="..." />
+                         <h5 className="card-title">
+                           <span className="col-6">ADDITIONAL SERVICES</span>
+                         </h5>
+                         <p className="card-text ">
+                           <span className=" col-4">Nail Repair_ _ _ $5 + </span><br/>
+                           <span className=" col-7">French Manicure_ _ _ $5 +</span><br/>
+                           <span className=" col-4">Nail Art (2 Fingers)_ _ _$10+  </span><br/>
+                           <span className=" col-6">Take Off Acrylic_ _ _$10 </span><br/>
+                           <span className=" col-8">Polish Change Hands (Reg)_ _ _$10 </span><br/>
+                           <span className=" col-8">Polish Change Hands (Gel Polish)_ _ _$10 </span><br/>
+                           <span className=" col-8">Polish Change Feet (Reg) (Reg)_ _ _$10 </span><br/>
+                         </p>
+                       </div>
+                  </div>
+                  <div className="card col-3" >
+                       <div className="card-body">
+                       <img src="https://www.byrdie.com/thmb/mf8ddOqnm8XsfWh-NkBBEVSdJz4=/600x600/smart/filters:no_upscale()/eyelashes-a993338dc98940b6af3a715eae3935cc.jpg" className="card-img-top" alt="..." />
+                         <h5 className="card-title">
+                           <span className="col-6">EYELASH EXTENSION</span>
+                         </h5>
+                         <p className="card-text ">
+                           <span className=" col-4">Full Set (Classic, Hybrid, Volume)_ _ _ $200 </span><br/>
+                           <span className=" col-7">Fill_ _ _ $50 +</span><br/>
+                           <span className=" col-4">Eyelash (Cluster)_ _ _$50+  </span><br/>
+                           <span className=" col-6">Strip - Lash_ _ _$30 </span><br/>
+                         </p>
+                       </div>
+                  </div>
+                   <div className="col-1"></div>
+            </div>  
         </div>
     )
 }
