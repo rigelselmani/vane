b358eafbebddb6ab653e823833fa2dec1d7ae4b2
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-10-29 00:07:11 -0400

    positioning the gallery

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -2,7 +2,7 @@ export default function Gallery(){
     return(
     <div className="container">
        <div className="row mt-5">
-           <div className="card col-3 row" style={{width: "18rem"}}>
+           <div className="card col-3 mx-1" style={{width: "18rem"}}>
              <img src="" className="card-img-top" alt="collection" />
              <div className="card-body">
                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
