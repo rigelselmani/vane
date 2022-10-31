8d69b5074429e8d1105fe146c1239fa5eebc6f0c
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-10-31 12:24:46 -0400

    placeholder at the form in contact page

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -6,15 +6,15 @@ export default function Contact(){
        <form className="col-5 contact-form my-5 pt-5" style={{martinTop:""}}>
           <div className="mb-3 mt-5">
             <label for="exampleInputEmail1" className="form-label">Name</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Name" aria-describedby="emailHelp" />
           </div>
           <div className="mb-3">
             <label for="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email@example.com"aria-describedby="emailHelp" />
           </div>
           <div className="mb-3">
             <label for="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" />
+            <input type="password" className="form-control" id="exampleInputPassword1"placeholder="Password" />
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
