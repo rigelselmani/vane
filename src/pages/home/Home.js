1e49e592f71d623cbb85cd7825012d626f0711fe
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-11-01 11:27:26 -0400

    subscription form connected with database

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -10,21 +10,21 @@ export default  function Home (){
               <img src="https://img.grouponcdn.com/deal/4DpuND4oxQjsme5VLb9F6K/451835441-2048x1229/v1/t300x182.jpg" className="img-thumbnail card-img-top w-25" style={{height:"230px"}} alt="" />
               <img src="http://s24193.pcdn.co/wp-content/uploads/2017/08/nail-salons-near-me-ENTITY-Instagram-lipstickmixtapes-e1504122361297.png" className=" img-thumbnail card-img-top w-25" style={{height:"320px"}} alt=""/>
               <div className="title-name">
-                <h1 className="display-4 font-weight-bold fs-2"><span className="text-danger">Vanessa</span> <span class="text-dark">Nail Tech</span></h1>
+                <h1 className="display-4 font-weight-bold fs-2"><span className="text-danger">Vanessa</span> <span className="text-dark">Nail Tech</span></h1>
                 <p className="lead display-4 fs-2">Professional Nail Art Tech</p>
                 <a className="contact-icon text-white fs-2" style={{textDecoration:"none"}} href="tel:703-870-6927" rel="noopener noreferrer">703-870-6927</a>
                 
               </div>
            </div>
-           <div class="row mb-4 ml-4">
-                <div class="col-md-3 "></div>
-                <div class="col-md-2">
+           <div className="row mb-4 ml-4">
+                <div className="col-md-3 "></div>
+                <div className="col-md-2">
                   <img className=" card-img-top d-none-sm rounded-circle" src="https://tse2.mm.bing.net/th?id=OIP.bk83NuPnjJT96jhD_gDmJQHaLH&pid=Api&P=0&w=300&h=300" alt="" />
                 </div>
-                <div class="col-md-2">  
+                <div className="col-md-2">  
                   <img  className=" card-img-top rounded-circle" src="https://tse3.mm.bing.net/th?id=OIP.W3r_SUK7ujIXc5KzlHo5eAHaHa&pid=Api&P=0&w=300&h=300" alt="" />
                 </div> 
-                <div  class="col-md-2">   
+                <div  className="col-md-2">   
                   <img className=" card-img-top rounded-circle" src="https://stayglam.com/wp-content/uploads/2019/04/Beautiful-Embellished-Pink-Nails.jpg" alt="" />
                 </div>
                 <div className="col-md-3"></div>
--- src/pages/home/Register.js
+++ src/pages/home/Register.js
@@ -1,11 +1,27 @@
+import { projectFirestore } from "../../firebase/config";
+import { useState } from "react";
 
 export default function Register(){
+
+    const [subscribe,setSubscribe]=useState("");
+
+    const handleSubmit = async (e) =>{
+      e.preventDefault();
+
+      try {
+        await projectFirestore.collection("subscriptions").add({subscribe}) 
+      }catch(err){
+          console.log(err)
+      }
+      console.log(subscribe)
+      setSubscribe("")
+    } 
     return(
         <div className="card-body mb-4">
-            <form className="subscribe-form">
+            <form className="subscribe-form" onSubmit={handleSubmit}>
               <div className=" mx-3 form-group mx-auto">
                 <label for="exampleInputEmail1">Subscribe to recieve cupons.</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" name="subscribe"    aria-describedby="emailHelp" placeholder="Enter your email address" />
+                <input onChange={((e)=>setSubscribe(e.target.value))} value={subscribe} type="email" className="form-control" id="exampleInputEmail1" name="subscribe"    aria-describedby="emailHelp" placeholder="Enter your email address" />
               </div>
               <button type="submit" className="btn btn-primary mt-2">Subscribe</button>
             </form>
