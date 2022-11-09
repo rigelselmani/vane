3e9c240a5e22a391b64ba0b6526a038711d72a86
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-11-09 08:41:51 -0500

    deploying to firebase and making responsive design

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,29 +5,29 @@ import "./Home.css"
 export default  function Home (){
     return (  
         <div className="jumbo row mx-0 mt-5 ">
-           <div className="jumbotron col-md-12 shadow-lg  pt-5 mb-5 bg-white rounded text-center text-white">
-              <img src="http://images8.design-editor.com/92/9293685/3958/8781D98C-E7A0-8B28-BED4-86520B3C7B90.jpg" className="img-thumbnail card-img-top w-25" style={{height:"170px"}} alt=""/>
-              <img src="https://img.grouponcdn.com/deal/4DpuND4oxQjsme5VLb9F6K/451835441-2048x1229/v1/t300x182.jpg" className="img-thumbnail card-img-top w-25" style={{height:"230px"}} alt="" />
-              <img src="http://s24193.pcdn.co/wp-content/uploads/2017/08/nail-salons-near-me-ENTITY-Instagram-lipstickmixtapes-e1504122361297.png" className=" img-thumbnail card-img-top w-25" style={{height:"320px"}} alt=""/>
+           <div className="jumbotron col-md-12 sm-mt-0 shadow-lg  pt-5 mb-5 bg-white rounded text-center text-white">
+              <img src="http://images8.design-editor.com/92/9293685/3958/8781D98C-E7A0-8B28-BED4-86520B3C7B90.jpg" className="img-thumbnail d-none d-md-inline d-lg-inline card-img-top w-25" style={{height:"170px"}} alt=""/>
+              <img src="https://img.grouponcdn.com/deal/4DpuND4oxQjsme5VLb9F6K/451835441-2048x1229/v1/t300x182.jpg" className="img-thumbnail d-none d-md-inline d-lg-inline card-img-top w-25" style={{height:"230px"}} alt="" />
+              <img src="http://s24193.pcdn.co/wp-content/uploads/2017/08/nail-salons-near-me-ENTITY-Instagram-lipstickmixtapes-e1504122361297.png" className=" img-thumbnail d-none d-md-inline d-lg-inline card-img-top w-25" style={{height:"320px"}} alt=""/>
               <div className="title-name">
-                <h1 className="display-4 font-weight-bold fs-2"><span className="text-danger">Vanessa</span> <span className="text-dark">Nail Tech</span></h1>
+                <h1 className="display-4 font-weight-bold fs-2"><span className="text-danger">Pure</span> <span className="text-dark">Nail Tech</span></h1>
                 <p className="lead display-4 fs-2">Professional Nail Art Tech</p>
                 <a className="contact-icon text-white fs-2" style={{textDecoration:"none"}} href="tel:703-870-6927" rel="noopener noreferrer"><i className="fa-regular fa-phone"></i>703-870-6927</a>
                 
               </div>
            </div>
-           <div className="row mb-4 ml-4">
-                <div className="col-md-3 "></div>
-                <div className="col-md-2">
-                  <img className=" card-img-top d-none-sm rounded-circle" src="https://tse2.mm.bing.net/th?id=OIP.bk83NuPnjJT96jhD_gDmJQHaLH&pid=Api&P=0&w=300&h=300" alt="" />
+           <div className=" row mb-4 ">
+                <div className=" round-img col-lg-3  d-none d-md-block d-lg-block"></div>
+                <div className="col-lg-2 d-none d-sm-none d-lg-block">
+                  <img className=" card-img-top rounded-circle" src="https://tse2.mm.bing.net/th?id=OIP.bk83NuPnjJT96jhD_gDmJQHaLH&pid=Api&P=0&w=300&h=300" alt="" />
                 </div>
-                <div className="col-md-2">  
+                <div className="col-lg-2  d-none d-lg-block">  
                   <img  className=" card-img-top rounded-circle" src="https://tse3.mm.bing.net/th?id=OIP.W3r_SUK7ujIXc5KzlHo5eAHaHa&pid=Api&P=0&w=300&h=300" alt="" />
                 </div> 
-                <div  className="col-md-2">   
+                <div  className="col-lg-2   d-block">   
                   <img className=" card-img-top rounded-circle" src="https://stayglam.com/wp-content/uploads/2019/04/Beautiful-Embellished-Pink-Nails.jpg" alt="" />
                 </div>
-                <div className="col-md-3"></div>
+                <div className="col-lg-3 d-none d-lg-bock d-md-block"></div>
             </div>
             <Register />
         </div> 
--- src/pages/home/Register.js
+++ src/pages/home/Register.js
@@ -21,8 +21,8 @@ export default function Register(){
     return(
         <div className="card-body mb-4">
             <form className="subscribe-form" onSubmit={handleSubmit}>
-              <div className=" mx-3 form-group mx-auto">
-                <label for="exampleInputEmail1">Subscribe to recieve cupons.</label>
+              <div className=" mx-1 form-group mx-auto">
+                <label htmlFor="exampleInputEmail1">Subscribe to recieve cupons.</label>
                 <input onChange={((e)=>setSubscribe(e.target.value))} value={subscribe} type="email" className="form-control" id="exampleInputEmail1" name="subscribe"    aria-describedby="emailHelp" placeholder="Enter your email address" />
               </div>
               <button type="submit" className="btn mt-2">Subscribe</button>
